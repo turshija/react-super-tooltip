fb1962c6c01535884fb257cab1cd89354727f866
Boris Vujicic <turshija@gmail.com>
2020-12-21 17:10:47 +0100

    Add show/hide tooltip buttons in demo

--- test/index.js
+++ test/index.js
@@ -2,20 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactSuperTooltip from '../src';
 
+let tooltipRef;
+
 ReactDOM.render(
-  <div style={{ width: 1000, height: 1000, background: 'black', position: 'relative' }}>
-    <div style={{ width: 300, height: 300, marginTop: 100, marginLeft: 100, background: 'green', position: 'relative' }} id='container'>
-      <ReactSuperTooltip
-        component='div'
-        trigger='hover'
-        bounds='#container'
-        onShow={() => console.log('show')}
-        content='test test test test'
-        tooltipClassName='tooltip-class'
-        tooltipContainerClassName='root-class'
-        style={{ height: 50, width: 50, background: 'blue', position: 'absolute', top: 200, left: 200, whiteSpace: 'nowrap' }}>
-        <div/>
-      </ReactSuperTooltip>
+  <>
+    <div>
+      <button onClick={() => setTimeout(() => tooltipRef.show(), 0)}>show tooltip</button>
+      <button onClick={() => tooltipRef.hide()}>hide tooltip</button>
     </div>
-  </div>,
+
+    <div style={{ width: 1000, height: 1000, background: 'black', position: 'relative' }}>
+      <div style={{ width: 300, height: 300, marginTop: 100, marginLeft: 100, background: 'green', position: 'relative' }} id='container'>
+        <ReactSuperTooltip
+          component='div'
+          trigger='hover'
+          ref={(ref) => tooltipRef = ref}
+          bounds='#container'
+          onShow={() => console.log('show')}
+          content='test test test test'
+          tooltipClassName='tooltip-class'
+          tooltipContainerClassName='root-class'
+          style={{ height: 50, width: 50, background: 'blue', position: 'absolute', top: 200, left: 200, whiteSpace: 'nowrap' }}>
+          <div/>
+        </ReactSuperTooltip>
+      </div>
+    </div>
+  </>,
   document.getElementById('root'));
